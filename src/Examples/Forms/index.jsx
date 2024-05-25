7b48bb87ba065007fab3a978ca88b228d462a733
Tushar Arora <tushar.arora1@housing.com>
2024-05-25 16:09:19 +0530

    Form Hooks Implemented

--- src/Examples/Forms/index.jsx
+++ src/Examples/Forms/index.jsx
@@ -9,7 +9,7 @@ function Forms() {
     <Fragment>
       {/* <Action /> */}
       {/* <UseActionStateTest /> */}
-
+      {/* <UseFormStateTest /> */}
       <UseOptimisticTest />
     </Fragment>
   );
