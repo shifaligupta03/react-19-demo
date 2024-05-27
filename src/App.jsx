38d77ad0da91bbbe1933abd8fa4d7812ba1d99a9
anapallimahipritamreddy <anapalli.reddy@housing.com>
2024-05-27 11:26:54 +0530

    forward ref changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,23 @@ import BasicCounter from "./Examples/BasicCounter";
 // import CounterWithSortList from "./Examples/CounterWithSortList";
 // import UsersExample from './Examples/Users';
 // import ThemeExample from './Examples/Theme';
+import ForwardRef from "./Examples/ForwardRef";
 
 function App() {
-
   return (
     <>
-     <h1>React 19 Examples</h1>
-    <div>
-      <Suspense
-        fallback={
-          <h1 className="loadingStyle">Loading...</h1>
-        }
-      >
-        <BasicCounter />
-        {/* <CounterWithSortList /> */}
-        {/* <Users /> */}
-        {/* <ThemeExample /> */}
-      </Suspense>
-    </div>
-  </>
-  )
-
-
+      <h1>React 19 Examples</h1>
+      <div>
+        <Suspense fallback={<h1 className="loadingStyle">Loading...</h1>}>
+          {/* <BasicCounter /> */}
+          <ForwardRef />
+          {/* <CounterWithSortList /> */}
+          {/* <Users /> */}
+          {/* <ThemeExample /> */}
+        </Suspense>
+      </div>
+    </>
+  );
 }
 
 export default App;
